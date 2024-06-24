376afd6c3a25ae2019de0db6fb8d0a73a91da33e
wangkaiyi <kaiyiwang7@163.com>
2024-06-24 15:23:46 +0800

    build

--- socket/single_conn_client.c
+++ socket/single_conn_client.c
@@ -1,3 +1,96 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+
+#define handle_error(cmd,result)\
+    if(result<0)\
+    {\
+        perror(cmd);\
+        return -1;\
+    }\
+
+void* read_from_server(void* arg){
+    int sockfd = *(int*)arg;
+    char* buffer = malloc(1024*sizeof(char));
+    ssize_t count = 0;
+    while((count = recv(sockfd, buffer, 1024, 0)) > 0){
+        if(count<0){
+            perror("recv");
+        }
+        fputs(buffer,stdout);
+        
+    }
+    printf("客户端请求关闭连接....\n");
+    free(buffer);
+    return NULL;
+
+}
+
+void* write_to_server(void* arg){
+    int sockfd = *(int*)arg;
+    char* buffer = malloc(1024*sizeof(char));
+    ssize_t count = 0;
+    while(fgets(buffer,sizeof(buffer),stdin)!=NULL){
+        count = send(sockfd,buffer,1024,0);
+        if(count<0){
+            perror("send");
+        }
+    }
+    
+    printf("接收到命令行的终止信号，不再写入，关闭连接....\n");
+    shutdown(sockfd,SHUT_WR);
+    free(buffer);
+    return NULL;
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int sockfd,temp_result;
+    pthread_t pid_read,pid_write;
+    struct sockaddr_in client_addr,server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
+
+    server_addr.sin_family = AF_INET;
+    // 连接本机 127.0.0.
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    // 连接端口 6666
+    server_addr.sin_port = htons(6666);
+    client_addr.sin_family = AF_INET;
+    // 连接本机 192.168.10.150
+    client_addr.sin_addr.s_addr = inet_addr("172.21.242.84");
+    // 连接端口 8888
+    client_addr.sin_port = htons(8888);
+
+    sockfd = socket(AF_INET,SOCK_STREAM,0);
+    
+    temp_result = bind(sockfd,(struct sockaddr*)&client_addr,sizeof(client_addr));
+    handle_error("bind",temp_result);
+    temp_result = connect(sockfd,(struct sockaddr *)&server_addr, sizeof(server_addr));
+    // temp_result = connect(sockfd,(struct sockaddr*)&server_addr,sizeof(server_addr));
+    handle_error("connectwkwkw",temp_result);
+
+    pthread_create(&pid_read,NULL,&read_from_server,&sockfd);
+    pthread_create(&pid_write,NULL,&write_to_server,&sockfd);
+
+    pthread_join(pid_read,NULL);
+    pthread_join(pid_write,NULL);
+
+    close(sockfd);
+    return 0;
+}
+
+
+
 
 
 
@@ -33,121 +126,113 @@
 // #include <unistd.h>
 // #include <pthread.h>
 
-// // 192.168.10.150 IP 地址的16进制表示
-// #define INADDR_LOCAL 0xC0A80A96
+
 
 // #define handle_error(cmd, result) \
-// if (result < 0) \
+// if (result < 0)\
 // { \
-// perror(cmd);    \
-// return -1;  \
+// perror(cmd);\
+// return -1;\
 // }
 
-// void *read_from_server(void *argv)
-// {
-//     int sockfd = *(int *)argv;
-//     char *read_buf = NULL;
-//     ssize_t count = 0;
 
-//     read_buf = malloc(sizeof(char) * 1024);
+// void* read_from_client(void* argv){
+//     //接收recv接收客户端的发送的数据，打印到控制台
+//     int fd = *(int*)argv;
+//     char* read_buf = malloc(sizeof(char)*1024);
+//     ssize_t count=0;
+
 //     if (!read_buf)
 //     {
-//         perror("malloc client read_buf");
+//         perror("malloc server read_buf");
 //         return NULL;
 //     }
-
-//     while (count = recv(sockfd, read_buf, 1024, 0))
+    
+//     while (count = recv(fd,read_buf,1024,0))
 //     {
-//         if (count < 0)
+//         if (count<0)
 //         {
-//         perror("recv");
+//             perror("recv");
 //         }
-//         fputs(read_buf, stdout);
+//         fputs(read_buf,stdout);
 //     }
-
-//     printf("收到服务端的终止信号......\n");
+//     printf("客户端请求关闭连接....\n");
 //     free(read_buf);
-
 //     return NULL;
+    
+
 // }
-
-// void *write_to_server(void *argv)
-// {
-//     int sockfd = *(int *)argv;
-//     char *write_buf = NULL;
-//     ssize_t send_count = 0;
-
-//     write_buf = malloc(sizeof(char) * 1024);
+// void* write_to_client(void* argv){
+//     //接收控制台的输入的信息，写出去
+//     int fd = *(int*)argv;
+//     char* write_buf = malloc(sizeof(char)*1024);
+//     ssize_t count=0;
 
 //     if (!write_buf)
 //     {
-//         printf("写缓存分配失败，断开连接\n");
-//         shutdown(sockfd, SHUT_WR);
-//         perror("malloc client write_buf");
-
+//         perror("malloc server write_buf");
 //         return NULL;
 //     }
-
-//     while (fgets(write_buf, 1024, stdin) != NULL)
+    
+//     while (fgets(write_buf,1024,stdin)!=NULL)
 //     {
-//         send_count = send(sockfd, write_buf, 1024, 0);
-//         if (send_count < 0)
+//         count = send(fd,write_buf,1024,0);
+//         if (count<0)
 //         {
-//         perror("send");
+//             perror("recv");
 //         }
+       
 //     }
-
-//     printf("接收到命令行的终止信号，不再写入，关闭连接......\n");
-//     shutdown(sockfd, SHUT_WR);
+//     printf("接收到控制台的关闭请求 不再写入....\n");
+//     shutdown(fd,SHUT_WR);
 //     free(write_buf);
-
 //     return NULL;
+
 // }
 
 // int main(int argc, char const *argv[])
 // {
-//     int sockfd, temp_result;
-//     pthread_t pid_read, pid_write;
+//     struct sockaddr_in server_addr,client_addr;
+//     int sockfd,temp_result,client_fd;
+//     pthread_t pid_read,pid_write;
+//     memset(&server_addr,0,sizeof(server_addr));
+//     memset(&client_addr,0,sizeof(client_addr));
 
-//     struct sockaddr_in server_addr, client_addr;
-
-//     memset(&server_addr, 0, sizeof(server_addr));
-//     memset(&client_addr, 0, sizeof(client_addr));
-
+//     //声明IPV4协议
 //     server_addr.sin_family = AF_INET;
-//     // 连接本机 127.0.0.1
-//     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-//     // 连接端口 6666
+//     //需要绑定0.0.0.0地址，转换成网络字节序后完成设置
+//     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);;
+//     //端口号随便用一个，但是不要使用特权端口
 //     server_addr.sin_port = htons(6666);
 
-//     client_addr.sin_family = AF_INET;
-//     // 连接本机 172.21.242.84
-//     client_addr.sin_addr.s_addr = inet_addr("172.21.242.84");
-//     // 连接端口 8888
-//     client_addr.sin_port = htons(8888);
+//     //创建socket
+//     sockfd = socket(AF_INET,SOCK_STREAM,0);
+//     handle_error("socket",sockfd);
 
-//     // 创建socket
-//     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//     handle_error("socket", sockfd);
+//     //绑定地址
+//     temp_result = bind(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr));
+//     handle_error("bind",temp_result);
 
-//     temp_result = bind(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
-//     handle_error("bind", temp_result);
+//     //进入监听模式
+//     temp_result = listen(sockfd,128);
+//     handle_error("listen",temp_result);
 
-//     // 连接server
-//     temp_result = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-//     handle_error("connect", temp_result);
+//     //接收第一个client链接
+//     socklen_t cliaddr_len = sizeof(client_addr);
 
-//     // 启动一个子线程，用来读取服务端数据，并打印到 stdout
-//     pthread_create(&pid_read, NULL, read_from_server, (void *)&sockfd);
-//     // 启动一个子线程，用来从命令行读取数据并发送到服务端
-//     pthread_create(&pid_write, NULL, write_to_server, (void *)&sockfd);
+//     client_fd = accept(sockfd,(struct sockaddr *)&client_addr,&cliaddr_len);
+//     handle_error("accept",client_fd);
 
-//     // 阻塞主线程
-//     pthread_join(pid_read, NULL);
-//     pthread_join(pid_write, NULL);
+//     printf("与客户端 from %s at PORT %d 文件描述符 %d 建立连接\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), client_fd);
 
-//     printf("关闭资源\n");
+//     pthread_create(&pid_read,NULL,read_from_client,(void*)&client_fd);
+//     pthread_create(&pid_write,NULL,write_to_client,(void*)&client_fd);
+
+//     pthread_join(pid_read,NULL);
+//     pthread_join(pid_write,NULL);
+
+//     printf("释放资源");
 //     close(sockfd);
-
+//     close(client_fd);
 //     return 0;
 // }
--- socket/single_conn_server.c
+++ socket/single_conn_server.c
@@ -8,113 +8,232 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define handle_error(cmd,result)\
+    if (result<0)\
+    {\
+        perror(cmd);\
+        return -1;\
+    }\
 
-
-#define handle_error(cmd, result) \
-if (result < 0)\
-{ \
-perror(cmd);\
-return -1;\
-}
-
-
-void* read_from_client(void* argv){
-    //接收recv接收客户端的发送的数据，打印到控制台
-    int fd = *(int*)argv;
-    char* read_buf = malloc(sizeof(char)*1024);
-    ssize_t count=0;
-
-    if (!read_buf)
-    {
-        perror("malloc server read_buf");
-        return NULL;
-    }
-    
-    while (count = recv(fd,read_buf,1024,0))
-    {
-        if (count<0)
-        {
+void* read_from_client(void* arg){
+    int sockfd = *(int*)arg;
+    char* buffer = malloc(1024*sizeof(char));
+    ssize_t count = 0;
+    while(count=recv(sockfd,buffer,1024,0)){
+        if(count<0){
             perror("recv");
         }
-        fputs(read_buf,stdout);
+        fputs(buffer,stdout);
+        
     }
     printf("客户端请求关闭连接....\n");
-    free(read_buf);
+    free(buffer);
     return NULL;
-    
 
 }
-void* write_to_client(void* argv){
-    //接收控制台的输入的信息，写出去
-    int fd = *(int*)argv;
-    char* write_buf = malloc(sizeof(char)*1024);
-    ssize_t count=0;
 
-    if (!write_buf)
-    {
-        perror("malloc server write_buf");
-        return NULL;
-    }
-    
-    while (fgets(write_buf,1024,stdin)!=NULL)
-    {
-        count = send(fd,write_buf,1024,0);
-        if (count<0)
-        {
-            perror("recv");
+void* write_to_client(void* arg){
+    int sockfd = *(int*)arg;
+    char* buffer = malloc(1024*sizeof(char));
+    ssize_t count = 0;
+    while(fgets(buffer,sizeof(buffer),stdin)!=NULL){
+        count = send(sockfd,buffer,1024,0);
+        if(count<0){
+            perror("send");
         }
-       
     }
-    printf("接收到控制台的关闭请求 不再写入....\n");
-    shutdown(fd,SHUT_WR);
-    free(write_buf);
+    
+    printf("接收到命令行的终止信号，不再写入，关闭连接....\n");
+    shutdown(sockfd,SHUT_WR);
+    free(buffer);
     return NULL;
-
+    
 }
-
 int main(int argc, char const *argv[])
 {
-    struct sockaddr_in server_addr,client_addr;
-    int sockfd,temp_result,client_fd;
+    int sock_fd,temp_result,client_fd;
     pthread_t pid_read,pid_write;
+    struct sockaddr_in server_addr,client_addr;
     memset(&server_addr,0,sizeof(server_addr));
     memset(&client_addr,0,sizeof(client_addr));
-
-    //声明IPV4协议
     server_addr.sin_family = AF_INET;
-    //需要绑定0.0.0.0地址，转换成网络字节序后完成设置
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);;
-    //端口号随便用一个，但是不要使用特权端口
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(6666);
 
-    //创建socket
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    handle_error("socket",sockfd);
-
-    //绑定地址
-    temp_result = bind(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr));
+    sock_fd = socket(AF_INET,SOCK_STREAM,0);
+    temp_result = bind(sock_fd,(struct sockaddr*)&server_addr,sizeof(server_addr));
     handle_error("bind",temp_result);
 
-    //进入监听模式
-    temp_result = listen(sockfd,128);
+    temp_result = listen(sock_fd,128);
     handle_error("listen",temp_result);
 
-    //接收第一个client链接
     socklen_t cliaddr_len = sizeof(client_addr);
-
-    client_fd = accept(sockfd,(struct sockaddr *)&client_addr,&cliaddr_len);
+    client_fd = accept(sock_fd,(struct sockaddr*)&client_addr,&cliaddr_len);
     handle_error("accept",client_fd);
+    printf("与客户端from %s at PORT%d 文件描述符%d 建立连接\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port),client_fd);
 
-    printf("与客户端 from %s at PORT %d 文件描述符 %d 建立连接\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), client_fd);
-
-    pthread_create(&pid_read,NULL,read_from_client,(void*)&client_fd);
-    pthread_create(&pid_write,NULL,write_to_client,(void*)&client_fd);
+    pthread_create(&pid_read,NULL,&read_from_client,&client_fd);
+    pthread_create(&pid_write,NULL,&write_to_client,&client_fd);
 
     pthread_join(pid_read,NULL);
     pthread_join(pid_write,NULL);
 
     printf("释放资源");
-    close(sockfd);
     close(client_fd);
+    close(sock_fd);
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <sys/socket.h>
+// #include <sys/types.h>
+// #include <netinet/in.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <arpa/inet.h>
+// #include <unistd.h>
+// #include <pthread.h>
+
+// // 192.168.10.150 IP 地址的16进制表示
+// #define INADDR_LOCAL 0xC0A80A96
+
+// #define handle_error(cmd, result) \
+// if (result < 0) \
+// { \
+// perror(cmd);    \
+// return -1;  \
+// }
+
+// void *read_from_server(void *argv)
+// {
+//     int sockfd = *(int *)argv;
+//     char *read_buf = NULL;
+//     ssize_t count = 0;
+
+//     read_buf = malloc(sizeof(char) * 1024);
+//     if (!read_buf)
+//     {
+//         perror("malloc client read_buf");
+//         return NULL;
+//     }
+
+//     while (count = recv(sockfd, read_buf, 1024, 0))
+//     {
+//         if (count < 0)
+//         {
+//         perror("recv");
+//         }
+//         fputs(read_buf, stdout);
+//     }
+
+//     printf("收到服务端的终止信号......\n");
+//     free(read_buf);
+
+//     return NULL;
+// }
+
+// void *write_to_server(void *argv)
+// {
+//     int sockfd = *(int *)argv;
+//     char *write_buf = NULL;
+//     ssize_t send_count = 0;
+
+//     write_buf = malloc(sizeof(char) * 1024);
+
+//     if (!write_buf)
+//     {
+//         printf("写缓存分配失败，断开连接\n");
+//         shutdown(sockfd, SHUT_WR);
+//         perror("malloc client write_buf");
+
+//         return NULL;
+//     }
+
+//     while (fgets(write_buf, 1024, stdin) != NULL)
+//     {
+//         send_count = send(sockfd, write_buf, 1024, 0);
+//         if (send_count < 0)
+//         {
+//         perror("send");
+//         }
+//     }
+
+//     printf("接收到命令行的终止信号，不再写入，关闭连接......\n");
+//     shutdown(sockfd, SHUT_WR);
+//     free(write_buf);
+
+//     return NULL;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     int sockfd, temp_result;
+//     pthread_t pid_read, pid_write;
+
+//     struct sockaddr_in server_addr, client_addr;
+
+//     memset(&server_addr, 0, sizeof(server_addr));
+//     memset(&client_addr, 0, sizeof(client_addr));
+
+//     server_addr.sin_family = AF_INET;
+//     // 连接本机 127.0.0.1
+//     server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+//     // 连接端口 6666
+//     server_addr.sin_port = htons(6666);
+
+//     client_addr.sin_family = AF_INET;
+//     // 连接本机 172.21.242.84
+//     client_addr.sin_addr.s_addr = inet_addr("172.21.242.84");
+//     // 连接端口 8888
+//     client_addr.sin_port = htons(8888);
+
+//     // 创建socket
+//     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+//     handle_error("socket", sockfd);
+
+//     temp_result = bind(sockfd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+//     handle_error("bind", temp_result);
+
+//     // 连接server
+//     temp_result = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+//     handle_error("connect", temp_result);
+
+//     // 启动一个子线程，用来读取服务端数据，并打印到 stdout
+//     pthread_create(&pid_read, NULL, read_from_server, (void *)&sockfd);
+//     // 启动一个子线程，用来从命令行读取数据并发送到服务端
+//     pthread_create(&pid_write, NULL, write_to_server, (void *)&sockfd);
+
+//     // 阻塞主线程
+//     pthread_join(pid_read, NULL);
+//     pthread_join(pid_write, NULL);
+
+//     printf("关闭资源\n");
+//     close(sockfd);
+
+//     return 0;
+// }
