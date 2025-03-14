45fd03defccc60c8f4d3a553208cd11982dcbb30
“zziyang” <kaiyiwang7@163.com>
2025-03-14 10:44:42 +0800

    hello world

--- helloworld/hello.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HELLO_H_
-#define __HELLO_H_
-
-void say_hello();
-
-
-
-#endif
--- helloworld/main.c
+++
@@ -1,7 +0,0 @@
-#include "hello.h"
-
-int main(){
-    say_hello();
-    printf("hello world \n");
-    return 0;
-}
